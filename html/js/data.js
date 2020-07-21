c8732616e6589ba3c1cecb88e15f62385ba25750
Adrien Barbot <catzhead@hotmail.com>
2020-07-21 22:53:20 +0200

    more generic data structure to generate multiple graphs in the same page

---
+++ html/js/data.js
@@ -0,0 +1,13 @@
+var graphData = {
+  'cy1': [
+    { data: {id: 'external1'} },
+    { data: {id: 'external2'} },
+    { data: {id: 'extedge1', source: 'external1',target: 'external2'} },
+    { data: {id: 'extedge2', source: 'external2',target: 'external2'} }
+  ],
+  'cy2': [
+    { data: {id: 'external3'} },
+    { data: {id: 'external4'} },
+    { data: {id: 'extedge3', source: 'external3',target: 'external4'} }
+  ]
+};
