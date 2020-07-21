58423ef938e166b0555af0b5c52bea3031aaa670
Adrien Barbot <catzhead@hotmail.com>
2020-07-21 22:53:50 +0200

    first test at data generated from python

---
+++ html/js/data_generated.js
@@ -0,0 +1,9 @@
+var externalData = [
+{"data": {"id": "a"}},
+{"data": {"id": "b"}},
+{"data": {"id": "edge0", "source": "a", "target": "b"}},
+{"data": {"id": "c"}},
+{"data": {"id": "edge1", "source": "b", "target": "c"}},
+{"data": {"id": "d"}},
+{"data": {"id": "edge2", "source": "c", "target": "d"}},
+{"data": {"id": "edge3", "source": "d", "target": "a"}}];
