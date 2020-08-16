6cd14921d24bd7bb1a7e5c3d6eee5ec04262e4be
Adrien Barbot <catzhead@hotmail.com>
2020-08-16 15:42:19 +0200

    add basic author bio and blog post pages

--- django/site1/blog/urls.py
+++ django/site1/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('author/<int:author_id>', views.author_bio, name='author_bio'),
+    path('blogpost/<int:blogpost_id>', views.blogpost, name='blogpost')
 ]
--- django/site1/blog/views.py
+++ django/site1/blog/views.py
@@ -1,7 +1,17 @@
-from blog.models import BlogPost
-from django.shortcuts import render
+from blog.models import Author, BlogPost
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
     blog_posts = BlogPost.objects.all()
     return render(request, 'blog/index.html', {'blog_posts': blog_posts})
+
+
+def author_bio(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'blog/author.html', {'author': author})
+
+
+def blogpost(request, blogpost_id):
+    blog_post = get_object_or_404(BlogPost, pk=blogpost_id)
+    return render(request, 'blog/blogpost.html', {'blog_post': blog_post})
